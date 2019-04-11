0427ad2318d0a9ca5793b5835076dee12cfc15bd
kei.taneishi <kei.taneishi@riken.jp>
2019-04-11 09:43:15 +0900

    use MKL_DNN

--- code/classification/run_training.sh
+++ code/classification/run_training.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#SBATCH --partition xdg-003-compute
+
+export OMP_NUM_THREADS=10
+export MKL_NUM_THREADS=$OMP_NUM_THREADS
 
 DATASET=HIV
 # DATASET=yourdata
@@ -19,5 +23,6 @@ decay_interval=50
 
 iteration=500
 
+echo $OMP_NUM_THREADS
 setting=$DATASET--radius$radius--dim$dim--layer$layer--batch$batch--lr$lr--lr_decay$lr_decay--decay_interval$decay_interval
 python run_training.py $DATASET $radius $dim $layer $batch $lr $lr_decay $decay_interval $iteration $setting
